031faea8f3b74128cd6ed0230b296cdcf0b54225
NB11B <124950290+NB11B@users.noreply.github.com>
2026-09-15 00:31:10 -0400

    Use Lean lakefile for nanoda module detection

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package MAPEOGEOFormal
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.1"
+
+@[default_target]
+lean_lib MAPEOGEOFormal
